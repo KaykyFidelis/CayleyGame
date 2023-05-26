00c9255da52d08622b458037cc919d54f9024b54
KaykyFidelis <kaykyfidelisserafim@gmail.br>
2023-05-25 22:01:18 -0300

    Adição das últimas telas iniciais

--- web/src/pages/AneisPage/AneisPage.js
+++ web/src/pages/AneisPage/AneisPage.js
@@ -6,14 +6,27 @@ const AneisPage = () => {
     <>
       <MetaTags title="Aneis" description="Aneis page" />
 
-      <h1>AneisPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/AneisPage/AneisPage.js</code>
-      </p>
-      <p>
-        My default route is named <code>aneis</code>, link to me with `
-        <Link to={routes.aneis()}>Aneis</Link>`
-      </p>
+      <div id="container">
+        <a href={routes.modeGame()}><h3 id="barra_superior">atividade - fmcc II</h3></a>
+        <img src="/logo_ufcg.png" alt="logo"></img>
+      </div>
+
+      <h1 id="titleSelectMode">Aneis</h1>
+
+      <div id="centroAneis">
+        <h2 id="tituloJogo">Selecione o nível do jogo</h2>
+        <button id="botaoFacil" type="button">
+          <a href={routes.aneis()}><h2>Fácil</h2></a>
+        </button>
+        <button id="botaoMedio" type="button">
+        <a href={routes.aneis()}><h2>Médio</h2></a>
+        </button>
+        <button id="botaoDificil" type="button">
+        <a href={routes.aneis()}><h2>Dificil</h2></a>
+        </button>
+      </div>
+      <img id="appleMangoAneis" src="/apple_mango.png" alt="logo"></img>
+      <img id="strawberryAneis" src="/strawberry.png" alt="logo"></img>
     </>
   )
 }
--- web/src/pages/GruposPage/GruposPage.js
+++ web/src/pages/GruposPage/GruposPage.js
@@ -5,15 +5,27 @@ const GruposPage = () => {
   return (
     <>
       <MetaTags title="Grupos" description="Grupos page" />
+      <div id="container">
+        <a href={routes.modeGame()}><h3 id="barra_superior">atividade - fmcc II</h3></a>
+        <img src="/logo_ufcg.png" alt="logo"></img>
+      </div>
 
-      <h1>GruposPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/GruposPage/GruposPage.js</code>
-      </p>
-      <p>
-        My default route is named <code>grupos</code>, link to me with `
-        <Link to={routes.grupos()}>Grupos</Link>`
-      </p>
+      <h1 id="titleSelectMode">Grupos</h1>
+
+      <div id="centroGrupos">
+        <h2 id="tituloJogo">Selecione o nível do jogo</h2>
+        <button id="botaoFacil" type="button">
+          <a href={routes.grupos()}><h2>Fácil</h2></a>
+        </button>
+        <button id="botaoMedio" type="button">
+        <a href={routes.grupos()}><h2>Médio</h2></a>
+        </button>
+        <button id="botaoDificil" type="button">
+        <a href={routes.grupos()}><h2>Dificil</h2></a>
+        </button>
+      </div>
+      <img id="appleMangoGrupos" src="/apple_mango.png" alt="logo"></img>
+      <img id="strawberryGrupos" src="/strawberry.png" alt="logo"></img>
     </>
   )
 }
