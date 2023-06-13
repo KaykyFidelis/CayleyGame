53ded6cf1a635fc45ccef2802858c965ddd48803
Smilke <erikflorenci20@gmail.com>
2023-06-13 09:51:21 -0300

    modified:   api/package.json modified:   package.json modified:   web/package.json modified:   web/src/components/GameMenu/GameMenu.tsx modified:   yarn.lock

--- web/src/components/GameMenu/GameMenu.tsx
+++ web/src/components/GameMenu/GameMenu.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Container, Image, Row, Col } from 'react-bootstrap'
+import { Col, Container, Image } from 'react-bootstrap';
 
 import { routes } from '@redwoodjs/router'
 
@@ -8,65 +8,23 @@ import ButtonLink from '../ButtonLink/ButtonLink'
 
 const GameMenu = () => {
   return (
-    <div
-      className="d-flex align-items-center justify-content-center"
-      style={{
-        minHeight: 'calc(100vh - 56px)', // Subtract the height of the navbar
-      }}
-    >
-      <Container
-        className="position-relative d-flex flex-column align-items-center"
-        style={{
-          backgroundColor: '#6187DA',
-          width: '50%',
-          height: 'calc(70vh)',
-          borderRadius: '10px',
-          paddingTop: '56px', // Height of the navbar
-        }}
-      >
-        <Image
-          src="/strawberry.png"
-          alt="Logo"
-          style={{
-            width: '10%',
-            position: 'absolute',
-            top: '0',
-            right: '0',
-          }}
-        />
-        <Image
-          src="/apple_mango.png"
-          alt="Logo"
-          style={{
-            width: '24%',
-            position: 'absolute',
-            bottom: '2%',
-            left: '2%',
-          }}
-        />
-
-        <div className="mb-md-5 p-3">
-          <h1 className="game-title">CayleyGame</h1>
+    <div className="d-flex align-items-center justify-content-center" style={{ height: '92vh' }}>
+      <Container className="position-relative d-flex flex-column align-items-center justify-content-center" style={{ backgroundColor: '#6187DA', width: '50%', height: '70%', borderRadius: '10px' }}>
+        <Image src="/strawberry.png" alt="Logo" className="position-absolute top-0 end-0" style={{ width: '10%' }} />
+        <Image src="/apple_mango.png" alt="Logo" className="position-absolute bottom-2 start-0" style={{ width: '24%' }} />
+        <div className="mt-10">
+          <h1 className="text-center text-white" style={{ fontSize: '3rem' }}>CayleyGame</h1>
         </div>
-        <Row className="w-75 align-items-center justify-content-center mt-md-5">
-          <Col className="d-flex align-items-center justify-content-center">
-            <ButtonLink
-              route={routes.grupos()}
-              value="Grupos"
-              className="btn-primary btn-lg"
-            />
+        <div className="d-flex align-items-center justify-content-center" style={{ height: '100%', width: '80%' }}>
+          <Col className="d-flex align-items-center justify-content-center w-100 h-25">
+            <ButtonLink route={routes.grupos()} value="Grupos" className={"d-flex align-items-center justify-content-center"}/>
           </Col>
-          <Col className="d-flex align-items-center justify-content-center">
-            <ButtonLink
-              route={routes.aneis()}
-              value="Anéis"
-              className="btn-primary btn-lg"
-            />
+          <Col className="d-flex align-items-center justify-content-center w-100 h-25">
+            <ButtonLink route={routes.aneis()} value="Anéis" className={"d-flex align-items-center justify-content-center"}/>
           </Col>
-        </Row>
+        </div>
       </Container>
     </div>
   )
 }
-
 export default GameMenu
